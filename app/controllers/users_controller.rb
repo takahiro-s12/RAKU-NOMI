2f91b97d959be22821b50535bc10610512866925
takahiro1202 <tk.tennis.w@gmail.com>
2021-01-06 02:18:21 +0000

    [Add]users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,13 @@ class UsersController < ApplicationController
   end
 
   def update
+    current_user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:family_name, :first_name, :email, :nickname, :favorite_food, :favorite_drink, :hated_food)
   end
 end
